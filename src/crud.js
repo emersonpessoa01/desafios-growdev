1c31f4d671447730e6766914d3efd77299278ab5
𝐄𝐦𝐞𝐫𝐬𝐨𝐧 𝐏𝐞𝐬𝐬𝐨𝐚 <60630092+emersonpessoa01@users.noreply.github.com>
2026-02-10 12:12:09 -0300

    first commit

---
+++ src/crud.js
@@ -0,0 +1,27 @@
+/* Descrição da Atividade
+Nesta atividade, você deverá desenvolver um programa em JavaScript responsável por realizar um CRUD (Create, Read, Update e Delete) de veículos.
+
+Todas as interações com o usuário devem ser realizadas exclusivamente através de prompt, e as informações devem ser exibidas utilizando console.log e/ou alert, conforme necessário.
+ */
+
+/* Requisitos Gerais
+Cada veículo deve possuir os seguintes dados:
+
+Identificador (ID)
+Modelo
+Marca
+Ano
+Cor
+Preço
+
+Os veículos devem ser armazenados em uma lista (array).
+
+Cada veículo deve possuir um identificador único, gerado automaticamente pelo sistema.
+
+A lista de veículos deve estar ordenada pelo preço.
+
+A funcionalidade de filtro deve ser realizada por marca e apresentar os veículos ordenados pelo preço.
+
+Apenas os campos cor e preço podem ser atualizados.
+
+O sistema deve sempre retornar ao menu inicial após a execução de uma ação. */
